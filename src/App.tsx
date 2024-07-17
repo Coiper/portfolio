b3292a794537cbf6d3334d38a8e3c63a8dc19a9c
Elliot Coiper <elliotcoiper@gmail.com>
2024-07-17 15:59:54 +0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
+import Navbar from "./components/Navbar.tsx";
+import About from "./components/About.tsx";
+import Skills from "./components/Skills.tsx";
+import Projects from "./components/Projects.tsx";
+import Contact from "./components/Contact.tsx";
+import Footer from "./components/Footer.tsx";
+import Hero from "./components/Hero.tsx";
+
+export default function App() {
   return (
     <>
-      <a href={"https://coiper.github.io/MovieSearcher/"}>Word</a>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
-
-export default App;
